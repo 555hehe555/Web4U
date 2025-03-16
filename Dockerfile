88f0e850cd07371262f54a126901380b083920ea
555hehe555 <123888406+555hehe555@users.noreply.github.com>
2025-03-16 09:57:12 +0200

    add docker

---
+++ Dockerfile
@@ -0,0 +1,35 @@
+FROM python:3.11
+
+SHELL ["/bin/bash", "-c"]
+
+# Set environment variables
+ENV PYTHONUNBUFFERED=1
+ENV PYTHONDONTWRITEBYTECODE=1
+
+# Install dependencies
+RUN pip install --upgrade pip
+
+RUN apt update && apt upgrade -y && \
+    apt install -qy gcc libjpeg-dev libxslt-dev \
+    libpq-dev libmariadb-dev libmariadb-dev-compat gettext cron openssh-client flake8 locales vim && \
+    apt clean && rm -rf /var/lib/apt/lists/*
+
+
+# Set the locale
+RUN useradd -rms /bin/bash yt && chmod 777 /opt /run
+
+WORKDIR /yt
+
+RUN mkdir /yt/static && mkdir /yt/media && chown -R yt:yt /yt && chmod -R 755 /yt
+
+COPY --chown=yt:yt . .
+
+RUN pip install -r requirements.txt
+
+RUN pip install psycopg2
+
+RUN apt-get update && apt-get install -y netcat-traditional
+
+USER yt
+
+CMD ["python", "manage.py", "runserver"]
--- myblog/settings.py
+++ myblog/settings.py
@@ -16,7 +16,8 @@ from pathlib import Path
 from sys import base_prefix
 
 import environ
-from django.conf.global_settings import AUTH_USER_MODEL
+from django.conf.global_settings import AUTH_USER_MODEL, STATIC_ROOT
+from dotenv import load_dotenv
 
 env = environ.Env(DEBUG=(bool, True))
 
@@ -30,7 +31,7 @@ print(BASE_DIR)
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d%qbx%5eb)*aghg#72#ol11uy@2$fal4z%8#x4p3(^^a@6(ib='
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
@@ -93,12 +94,27 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+load_dotenv()
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'db',  # контейнер з базою даних
+        'PORT': '5432',
     }
 }
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.getenv(BASE_DIR / 'DATABASE_NAME'),
+#     }
+# }
 
 
 # Password validation
@@ -146,8 +162,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -155,4 +171,5 @@ STATICFILES_DIRS = [STATIC_DIR]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = 'media'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')

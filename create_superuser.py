06c2a2c066b3c9ce839fb153fffc3f6988ca5808
555hehe555 <123888406+555hehe555@users.noreply.github.com>
2025-03-23 12:58:06 +0200

    add update post

--- create_superuser.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def create_superuser():
-    """Run the createsuperuser command in a new command line window."""
-    print("Creating superuser...")
-    os.system("start cmd /k venv\\Scripts\\python manage.py createsuperuser")
-
-if __name__ == "__main__":
-    create_superuser()

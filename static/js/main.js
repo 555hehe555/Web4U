30a51227a39653b89a713925801c3c2d5cb5a7c3
555hehe555 <123888406+555hehe555@users.noreply.github.com>
2025-07-08 15:32:08 +0300

    3/10 add placeholder to blog

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,6 @@
 const postContainer = document.querySelector(".own-container");
 
+
 async function getAllPosts() {
   try {
     const response = await fetch("/api/posts/");
@@ -12,33 +13,59 @@ async function getAllPosts() {
   }
 }
 
+async function getPostByID(id) {
+  try {
+    const response = await fetch(`/api/posts/${id}`);
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error("Error:", error);
+    return []; // Повертаємо порожній масив, щоб уникнути краху
+  }
+}
+
+
 function deleteMarkup() {
   postContainer.innerHTML = "";
 }
 
 function renderBlogPosts(posts) {
-
-  console.log(posts)
+  console.log(posts);
   return posts.results
-    .map(
-      ({ id, title, description, author, date }) =>
-        ` 
-          <div class="container-item">
-            <li class="post">
-                <a class="post-title post-item" href="${id}"><h3>${title}</h3></a>
-                <p class="post-description post-item">${description}</p>
-                <p class="post-author post-item">${author}</p>
-            </li>
-          </div>
-        `
-    )
+    .map(({ id, title, img, description, author, date }) => {
+      const imageSrc = img ? img : "/media/image/standart/dfault.png"; // заміни на свій плейсхолдер
+      return `
+        <div class="container-item">
+          <li class="post">
+              <a class="post-title post-item" href="${id}"><h3>${title}</h3></a>
+              <img class="post-image post-item" src="${imageSrc}" width="200px" height="200px" style="border-radius: 20px;">
+              <p class="post-description post-item">${description}</p>
+              <p class="post-author post-item">${author}</p>
+          </li>
+        </div>
+      `;
+    })
     .join("");
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  const postLinks = document.querySelectorAll(".post-title")
+  postLinks.forEach(link => {
+      link.addEventListener('click', function (event) {
+          event.preventDefault();
+          alert(link.href)
+      });
+  });
+});
+
 
 async function showBlogPage() {
   deleteMarkup();
   const data = await getAllPosts(); // ⬅️ Тепер чекаємо результат
   postContainer.insertAdjacentHTML("beforeend", renderBlogPosts(data));
+
+  console.log(document.querySelectorAll(".post-title"));
 }
 
 showBlogPage();

851f44608098603271d4ac362da127c252648dff
VaILeo <postribailo.valeri@gmail.com>
2026-04-06 18:54:53 +0300

    remove swagger warning from auth docs

--- documentation/login_user.py
+++ documentation/login_user.py
@@ -1,53 +1,64 @@
-from drf_spectacular.types import OpenApiTypes
-from drf_spectacular.utils import extend_schema, OpenApiResponse, OpenApiExample
-from rest_framework import status
+from drf_spectacular.utils import extend_schema, OpenApiResponse, OpenApiExample, inline_serializer
+from rest_framework import serializers, status
 
 from blog_api.serializers import LoginCustomUserSerializer
 
 
+MessageSerializer = inline_serializer(
+    name="MessageResponse",
+    fields={
+        "detail": serializers.CharField(),
+    },
+)
+
 login_user_list_doc = extend_schema(
-    tags=['Auth'],
+    tags=["Auth"],
     description="Login API",
     request=LoginCustomUserSerializer,
     responses={
         status.HTTP_200_OK: OpenApiResponse(
-            response=True,
+            response=MessageSerializer,
             description="Login successful",
             examples=[
                 OpenApiExample(
                     name="Success response",
                     value={"detail": "Login successful"},
+                    response_only=True,
+                    status_codes=[200],
                 )
             ],
         ),
         status.HTTP_400_BAD_REQUEST: OpenApiResponse(
-            response=True,
+            response=MessageSerializer,
             description="Login failed",
             examples=[
                 OpenApiExample(
                     name="Failed response",
                     value={"detail": "Invalid credentials"},
+                    response_only=True,
+                    status_codes=[400],
                 )
             ],
-        )
+        ),
     },
 )
 
 logout_user_list_doc = extend_schema(
-    tags=['Auth'],
+    tags=["Auth"],
     description="Logout API",
     request=None,
     responses={
         status.HTTP_200_OK: OpenApiResponse(
-            response=True,
+            response=MessageSerializer,
             description="Logout successful",
             examples=[
                 OpenApiExample(
                     name="Success response",
                     value={"detail": "Logout successful"},
+                    response_only=True,
+                    status_codes=[200],
                 )
             ],
         )
     },
 )
-

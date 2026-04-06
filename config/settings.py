baac44417aec9d32bb8b1af066c1adc9e8894c10
VaILeo <postribailo.valeri@gmail.com>
2026-04-06 18:27:00 +0300

    remove extra swagger auth fields

--- config/settings.py
+++ config/settings.py
@@ -119,6 +119,10 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
@@ -177,4 +181,5 @@ SPECTACULAR_SETTINGS = {
         "displayRequestDuration": True,
         "persistAuthorization": True,
     },
+    "AUTHENTICATION_WHITELIST": [],
 }

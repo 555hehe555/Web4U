7d917983465f0e20bcf24988afadb65f977118e0
555hehe555 <123888406+555hehe555@users.noreply.github.com>
2026-04-08 17:25:40 +0300

    update tasks

--- blog_api/apps.py
+++ blog_api/apps.py
@@ -2,6 +2,7 @@ from django.apps import AppConfig
 from django.db.models.signals import post_migrate
 from django.db.utils import OperationalError
 
+
 class BlogApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'blog_api'
